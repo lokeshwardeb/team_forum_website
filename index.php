05588ab749934c8ca5ac7bf3e970d72df8ce225f
lokeshwardeb <lokeshwardebdevelop@gmail.com>
2023-12-28 22:32:51 +0600

    added all the files and started the html file and its rearrangements and the html file developments

--- index.php
+++ index.php
@@ -5,8 +5,10 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $Routes = [
     '/' => __DIR__ . '/views/pages/views.index.php',
     '/dashboard' => __DIR__ . '/views/pages/views.index.php',
-    '/read_all_data' => __DIR__ . '/views/pages/views.read_all_data.php',
-    '/create_data' => __DIR__ . '/views/pages/views.create_data.php',
+    '/home' => __DIR__ . '/views/pages/views.home.php',
+    '/template_check' => __DIR__ . '/views/pages/template_check.html',
+    // '/read_all_data' => __DIR__ . '/views/pages/views.read_all_data.php',
+    // '/create_data' => __DIR__ . '/views/pages/views.create_data.php',
     // '/get_token' => __DIR__ . '/views/pages/views.get_token.php',
 
 ];
---
+++ views/pages/views.home.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../../config/conn.php';
+require_once __DIR__ . '/../../models/models.php';
+require_once __DIR__ . '/../../controllers/controllers.php';
+
+$controllers = new Controllers;
+
+
+
+
+?>
